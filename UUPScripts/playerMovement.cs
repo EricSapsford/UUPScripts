7de73e545bd7b7d8e92a541e7015414375263734
Eric Sapsford <esapsgit@gmail.com>
2024-08-09 20:18:43 -0400

    added left right player movement listeners

--- UUPScripts/playerMovement.cs
+++ UUPScripts/playerMovement.cs
@@ -7,6 +7,9 @@ public class playerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+
     // Start is called before the first frame update
     // void Start()
     // {
@@ -16,6 +19,18 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 200 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+         {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+         }
+
+        if (Input.GetKey("a"))
+         {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+         }
+
+
     }
 }
